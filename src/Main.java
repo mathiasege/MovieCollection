a46a14741e13912af51709663f1bae364e6d662f
mathiasege <macl0001@stud.kea.dk>
2024-10-23 09:57:33 +0200

    Vi har samme skabelon

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world! welcome to the filmsamling");
         UserInterface ui = new UserInterface();
         ui.game();
-        System.out.println("hello");
     }
 }
