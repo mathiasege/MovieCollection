58d2629624e5e2f2064c333ced873605b8bd44d0
BenjaminSierotaKEA <the.crazy.thing.is@gmail.com>
2024-10-23 10:29:27 +0200

    added UI, MovieCollection

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -1,7 +1,27 @@
+import java.util.Scanner;
+
 public class UserInterface {
 
-    public void game(){
+    private Scanner input = new Scanner(System.in);
 
+    public void game(){
+        boolean exit = false;
+        System.out.println("Welcome to the movie collection program");
+
+        while (!exit){
+            System.out.println("Please enter a command:");
+            System.out.println("ADD: add a new movie");
+
+            String userChoice = input.nextLine();
+            userChoice = userChoice.toUpperCase();
+
+            switch (userChoice){
+                case "ADD"-> addMovie();
+                default -> System.out.println("Please enter a valid command");
+            }
+        }
     }
 
-}
+    private void addMovie(){
+
+    }}
