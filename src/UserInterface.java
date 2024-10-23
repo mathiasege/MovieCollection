cac1df43819655cfeb2a4b9ab9d8492c3df6c593
mathiasege <macl0001@stud.kea.dk>
2024-10-23 10:36:38 +0200

    Small changes

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -5,18 +5,19 @@ public class UserInterface {
     private Scanner input = new Scanner(System.in);
 
     public void game(){
-        boolean exit = false;
+        String userChoice = "";
         System.out.println("Welcome to the movie collection program");
 
-        while (!exit){
+        while (!userChoice.equals("END")){
             System.out.println("Please enter a command:");
             System.out.println("ADD: add a new movie");
 
-            String userChoice = input.nextLine();
-            userChoice = userChoice.toUpperCase();
+            userChoice = input.nextLine();
+            userChoice.toUpperCase();
 
             switch (userChoice){
                 case "ADD"-> addMovie();
+                case "END" -> System.out.println("You're ending the game.");
                 default -> System.out.println("Please enter a valid command");
             }
         }
