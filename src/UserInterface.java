d15f152802df358783278fe5fe9d48c987018ad0
mathiasege <macl0001@stud.kea.dk>
2024-10-23 10:47:45 +0200

    Indsatte UI

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -1,7 +1,28 @@
+import java.util.Scanner;
+
 public class UserInterface {
 
-    public void game(){
+    private Scanner input = new Scanner(System.in);
 
+    public void game(){
+        String userChoice = "";
+        System.out.println("Welcome to the movie collection program");
+
+        while (!userChoice.equals("END")){
+            System.out.println("Please enter a command:");
+            System.out.println("ADD: add a new movie");
+
+            userChoice = input.nextLine();
+            userChoice.toUpperCase();
+
+            switch (userChoice){
+                case "ADD"-> addMovie();
+                case "END" -> System.out.println("You're ending the game.");
+                default -> System.out.println("Please enter a valid command");
+            }
+        }
     }
 
-}
+    private void addMovie(){
+
+    }}
