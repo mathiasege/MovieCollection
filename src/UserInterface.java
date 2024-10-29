59771ceafa5b3cb40695b2f6fb09d5cbae99aa21
mathiasege <macl0001@stud.kea.dk>
2024-10-29 19:01:34 +0100

    Opdateret try catch

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -178,16 +178,14 @@ public class UserInterface {
         return color;
     }
 
+
+    // Har opdateret denne:
     // Kontrol for min int.
     private int checkInt(Scanner scan) {
         // Hvis rigtigt er indtastet.
         try {
-            int length = Integer.parseInt(scan.nextLine());
-            return length;
-        } catch (InputMismatchException e) {
-            System.out.println("Please enter a valid number.");
-            return checkInt(scan);
-        } catch (NumberFormatException e) {
+            return Integer.parseInt(scan.nextLine());
+        } catch (InputMismatchException | NumberFormatException e) {
             System.out.println("Please enter a valid number.");
             return checkInt(scan);
         }
