e09a3c8f6429fe1b6b8cdede748c89688e965354
mathiasege <macl0001@stud.kea.dk>
2024-11-06 19:50:44 +0100

    Test virker

--- Test/MovieCollectionTest.java
+++ Test/MovieCollectionTest.java
@@ -1,19 +1,52 @@
 import models.MovieCollection;
 import models.Movie;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MovieCollectionTest {
+    private MovieCollection movieCollection;
+
+    public MovieCollectionTest(){
+        movieCollection = new MovieCollection();
+    }
+
+    @Test
+    void setupSampleMovieCollection() {
+        // Arrange
+        deleteFileContent();
+        movieCollection = new MovieCollection();
+        // Act
+        try {
+            movieCollection.addMovie("Harry Potter and the Philosophers Stone", "Benjamin Sierota", 2001, "Yes", 120, "Fantasy");
+            movieCollection.addMovie("Harry Potter and the Chamber of Secrets", "Benjamin Sierota", 2002, "Yes", 120, "Fantasy");
+            movieCollection.addMovie("Harry Potter and the Prisoner of Azkaban", "Benjamin Sierota", 2003, "Yes", 120, "Fantasy");
+            movieCollection.addMovie("Harry Potter and the Goblet of Fire", "Benjamin Sierota", 2004, "Yes", 120, "Fantasy");
+            movieCollection.addMovie("Harry Potter and the Order of the Pheonix", "Benjamin Sierota", 2005, "Yes", 120, "Fantasy");
+            movieCollection.addMovie("Harry Potter and the Half-Blood Prince", "Benjamin Sierota", 2006, "Yes", 120, "Fantasy");
+            movieCollection.addMovie("Harry Potter and the Deathly Hallows Part 1", "Benjamin Sierota", 2007, "Yes", 120, "Fantasy");
+            movieCollection.addMovie("Harry Potter and the Deathly Hallows Part 2", "Benjamin Sierota", 2008, "Yes", 120, "Fantasy");
+
+        } catch (IOException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+        //ASSERT:
+        int expectedResult = 8;
+        int actualResult = movieCollection.getMoviesSorted().size();
+        assertEquals(expectedResult, actualResult);
+    }
 
     @Test
     void addOneMovie() {
         try {
             //Arrange:
-            MovieCollection movieCollection = new MovieCollection();
+            deleteFileContent();
+            movieCollection = new MovieCollection();
             //Act:
             movieCollection.addMovie("Harry Potter and the Mystical Object", "H.C Carter", 2006, "Yes", 120, "Fantasy");
             //Assert:
@@ -29,7 +62,8 @@ class MovieCollectionTest {
     void addThreeMovies() {
         try {
             //Arrange:
-            MovieCollection movieCollection = new MovieCollection();
+            deleteFileContent();
+            movieCollection = new MovieCollection();
             //Act:
             for (int i = 0; i < 3; i++) {
                 movieCollection.addMovie("Harry Potter and the Mystical Object", "H.C Carter", 2006, "Yes", 120, "Fantasy");
@@ -48,7 +82,8 @@ class MovieCollectionTest {
     void addNoMovies() {
         try {
             //Arrange:
-            MovieCollection movieCollection = new MovieCollection();
+            deleteFileContent();
+            movieCollection = new MovieCollection();
             //Act:
             //NOTHING LOL
             //Assert:
@@ -61,62 +96,59 @@ class MovieCollectionTest {
     }
 
     @Test
-    void displayAllMovies() {
-    }
-
-    @Test
     void searchByTitleNoResults() {
-        try {
-            //Arrange:
-            MovieCollection movieCollection = new MovieCollection();
-            setupSampleMovieCollection();
-            //Act:
-            ArrayList<Movie> searchResults = movieCollection.searchByTitle("Batman");
-            int actualResult = searchResults.size();
-            //ASSERT:
-            int expectedResult = 0;
-            assertEquals(expectedResult, actualResult);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
+        //Arrange:
+        deleteFileContent();
+        movieCollection = new MovieCollection();
+        testData();
+        //Act:
+        ArrayList<Movie> searchResults = movieCollection.searchByTitle("Batman");
+        int actualResult = searchResults.size();
+        //ASSERT:
+        int expectedResult = 0;
+        assertEquals(expectedResult, actualResult);
     }
 
     @Test
     void searchByTitleOneResults() {
-        try {
-            //Arrange:
-            MovieCollection movieCollection = setupSampleMovieCollection();
-            //Act:
-            ArrayList<Movie> searchResults = movieCollection.searchByTitle("Pheonix");
-            int actualResult = searchResults.size();
-            //ASSERT:
-            int expectedResult = 8;
-            assertEquals(expectedResult, actualResult);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        //Arrange:
+        deleteFileContent();
+        movieCollection = new MovieCollection();
+        testData();
+        //Act:
+        ArrayList<Movie> searchResults = movieCollection.searchByTitle("P");
+        int actualResult = searchResults.size();
+        //ASSERT:
+        int expectedResult = 8;
+        assertEquals(expectedResult, actualResult);
     }
 
     @Test
     void searchByTitleManyResults() {
-        try {
-            //Arrange:
-            MovieCollection movieCollection = setupSampleMovieCollection();
-            //Act:
-            ArrayList<Movie> searchResults = movieCollection.searchByTitle("Harry Potter");
-            int actualResult = searchResults.size();
-            //ASSERT:
-            int expectedResult = 8;
-            assertEquals(expectedResult, actualResult);
+        //Arrange:
+        deleteFileContent();
+        movieCollection = new MovieCollection();
+        testData();
+        //Act:
+        ArrayList<Movie> searchResults = movieCollection.searchByTitle("Harry Potter");
+        int actualResult = searchResults.size();
+        //ASSERT:
+        int expectedResult = 8;
+        assertEquals(expectedResult, actualResult);
+    }
+
+    public void deleteFileContent(){
+        try (FileWriter writer = new FileWriter("Movies.txt", false)){
+            writer.write("title,director,yearCreated,isInColor,lengthInMinutes,genre");
+            writer.write(System.lineSeparator());
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
     }
 
-    MovieCollection setupSampleMovieCollection() throws IOException {
-        MovieCollection movieCollection = new MovieCollection();
+    void testData() {
+        movieCollection = new MovieCollection();
+
         try {
             movieCollection.addMovie("Harry Potter and the Philosophers Stone", "Benjamin Sierota", 2001, "Yes", 120, "Fantasy");
             movieCollection.addMovie("Harry Potter and the Chamber of Secrets", "Benjamin Sierota", 2002, "Yes", 120, "Fantasy");
@@ -126,11 +158,8 @@ class MovieCollectionTest {
             movieCollection.addMovie("Harry Potter and the Half-Blood Prince", "Benjamin Sierota", 2006, "Yes", 120, "Fantasy");
             movieCollection.addMovie("Harry Potter and the Deathly Hallows Part 1", "Benjamin Sierota", 2007, "Yes", 120, "Fantasy");
             movieCollection.addMovie("Harry Potter and the Deathly Hallows Part 2", "Benjamin Sierota", 2008, "Yes", 120, "Fantasy");
-
         } catch (IOException e) {
-            throw new IOException(e);
+            throw new RuntimeException(e.getMessage());
         }
-
-        return movieCollection;
     }
 }
